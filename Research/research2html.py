deab9e1b37431a9ee0fa2405096bbcd4702731b9
Tian Ooi <tianooi.to@gmail.com>
2022-05-04 16:28:22 -0600

    research page added sponsors

--- Research/research2html.py
+++ Research/research2html.py
@@ -11,6 +11,19 @@ from datetime import datetime
 import pandas as pd
 
 
+excel_path = '../CCWJ_Website.xlsx'
+def research2html(excel_path):
+    # read sheet named researchin excel file
+    data = pd.read_excel(excel_path, sheet_name="Research")
+    data.fillna('', inplace=True)
+        
+    
+    # write each section (section name, data)
+    for index, section in data.iterrows():
+        if section['Section']:
+            write_section(section['Section'], data, index)
+
+
 def write_section(section_name, data, index_start):
     # make filename the first word of section name, keeping only alphanumeric characters
     filename = "".join(x for x in section_name.split()[0].lower() if x.isalnum()) + '-text.html'
@@ -18,20 +31,23 @@ def write_section(section_name, data, index_start):
     # open file to write into
     f = open(filename, 'w')
     
+    # go through each row in the sheet
     for i, row in data.iloc[index_start:].iterrows():
-        print(row)
         
         # detect if next section has been reached, if so stop writing section
         if row['Section'] and row['Section'] != section_name:
             break
-    
+        
+        # go through each column for the row
         for header, value in row.items():
     
             if header.startswith('Section_Heading') and value:
                 f.write('<h4>' + value + '</h4>\n')
+                
             elif header.startswith('Text_Block') and value:
                 text = value.strip('\n').replace('\n', '<br>') # preserve newlines in html
                 f.write('<p>' + text + '</p>\n')
+                
             elif header.startswith('Image') and value:
                 # get image path, look in Assets/Research folder and section_name (with underscores) subfolder 
                 img_folder = os.path.join('../Assets/Research/', section_name.replace(' ', '_'))
@@ -44,8 +60,37 @@ def write_section(section_name, data, index_start):
                     f.write('<img src="' + img_path + '" class="img-scale mx-auto d-block">\n')
                 else:
                     print('cannot find image ' + value)
+                    
+            elif header.startswith('Sponsor') and value:
+                sponsor_num = header.split('_')[-1]
+                
+                if sponsor_num == '1': # for first sponsor create row div
+                    f.write('<div class="row mb-2 mt-1">\n\n')
+                
+                f.write('<div class="col-lg-4 col-md-6">\n<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm bg-white h-md-200 position-relative">\n<div class="col p-4 d-flex flex-column position-static">\n')
+                f.write('<h5 class="mb-3">'+ value +'</h5>\n</div>\n')
+                
+                if row['Logo_Sponsor_' + sponsor_num]:
+                    # find and write photo
+                    pic_folder = '../Assets/About_Us/Sponsors_Logos/' # folder containing all photos
+                    
+                    img_path = ''
+                    for filename in os.listdir(pic_folder):
+                        if filename.startswith(row['Logo_Sponsor_' + sponsor_num]):
+                            img_path = pic_folder + filename
+                    if img_path:
+                        f.write('<div class="equipment-pic col-auto d-flex overflow-hidden p-3">\n<img class="fill-img" src="' + img_path + '" alt="pic">\n</div>\n')
+                
+                f.write('</div>\n</div>\n')
+                
+                # for last sponsor close row div
+                next_sponsor = 'Sponsor_' + str(int(sponsor_num) + 1)
+                if not row[next_sponsor]:
+                    f.write('</div>\n\n')
+                
             elif header.startswith('Presenter') and value:
                 f.write('<p>Presenter: ' + value.strip('\n').replace('\n', '<br>') + '</p>\n')
+                
             elif header.startswith('Link') and value:
                 # only keep first 11 characters after watch?v= to get video id
                 video_id = value.split("watch?v=")[1][:11]
@@ -56,23 +101,4 @@ def write_section(section_name, data, index_start):
 
     f.close()
 
-excel_path = '../CCWJ_Website.xlsx'
-#def research2html(excel_path):
-    # read sheet named researchin excel file
-data = pd.read_excel(excel_path, sheet_name="Research")
-data.fillna('', inplace=True)
-    
-
-# write each section (section name, data)
-for index, section in data.iterrows():
-    # if section['Section'].startswith('Advisory Boards'):
-    #     # write special advisory page
-    #     write_advisory(section['Section'], section)
-    # elif section['Section'].startswith('Sponsors'):
-    #     # write special sponsors page
-    #     write_sponsors(section['Section'], section)
-    # else:
-        # write default page format
-    if section['Section']:
-        write_section(section['Section'], data, index)
     
