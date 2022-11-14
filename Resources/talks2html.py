f9dbf1f6f24e6b9446ae90b4b13fefdbda624043
Tian Ooi <tianooi.to@gmail.com>
2022-11-14 14:37:56 -0700

    resources/talks will look for pdf slides, added date

--- Resources/talks2html.py
+++ Resources/talks2html.py
@@ -7,8 +7,10 @@
 import os
 import pandas as pd
 
-
 excel_path = '../CCWJ_Website.xlsx'
+# folder with .pdf presentation slides, named starting with presentation code
+presentations_folder = '../Assets/Resources/Tech_Talks_Presentations'
+
 def talks2html(excel_path):
     # read sheet named resources_videos excel file
     data = pd.read_excel(excel_path, sheet_name="Resources_Talks")
@@ -26,11 +28,26 @@ def talks2html(excel_path):
             f.write('<h4>' + row['Presentation_Title'] + '</h4>\n')
         
         if row['Presenter']:
-            f.write('<p>Presenter: ' + row['Presenter'].strip('\n').replace('\n', '<br>') + '</p>\n')
+            f.write('<p>Presenter: ' + row['Presenter'].strip('\n').replace('\n', '<br>'))
+        
+        if row['Presentation_Code']: # look for pdf file starting with presentation_code in presentations_folder
+            for filename in os.listdir(presentations_folder):
+                if filename.startswith(str(row['Presentation_Code'])) and filename.endswith('.pdf'):
+                    f.write(' [<a target="_blank" href="' + os.path.join(presentations_folder,filename) + '">Slides</a>]</p>\n')
+                    break
+            else: # presentation code exists but pdf slides not found
+                f.write('</p>\n')
+                print('Resources/Talks: missing presentation slides code ' + row['Presentation_Code']) 
+        else: # if no presentation code, write closing p tag
+            f.write('</p>\n')
+
+        if row['Date']: # format date as Mar 4, 2014
+            date = row['Date'].strftime("%b %-d, %Y")
+            f.write('<p>Date: ' + date.strip('\n').replace('\n', '<br>') + '</p>\n')
             
         if row['Abstract']:
             text = row['Abstract'].strip('\n').replace('\n', '<br>') # preserve newlines in html
-            f.write('<p>' + text + '</p>\n')
+            f.write('<p>' + text + '</p>')
             
         if row['Link']: # youtube video
             link = row['Link']
