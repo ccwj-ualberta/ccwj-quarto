c8cfaef29bc1d0eb1c6edbd738810f17d05e025b
Tian Ooi <tianooi.to@gmail.com>
2022-12-20 15:49:40 -0700

    Updated posters2html to allow pdf posters with automatic thumbnail png generation

--- Research/posters2html.py
+++ Research/posters2html.py
@@ -8,6 +8,7 @@
 
 import os
 import pandas as pd
+import fitz
 
 poster_folder = '../Assets/Research/Posters/' # folder containing all posters as images
 
@@ -35,15 +36,28 @@ def write_section(csv_key, section_title, section_id, data, f):
 
         f.write('<div class="col-xl-4 col-md-6">\n<div class="row card g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm bg-white h-md-200 position-relative">\n')
         
-        # find and write photo with link
+        # find and write image or pdf with link
         img_path = ''
        
         for filename in os.listdir(poster_folder):
             if filename.startswith(row['Poster_Code']):
                 img_path = os.path.join(poster_folder,filename)
+                if filename.endswith('.pdf'): # for pdf files, generate image thumbnail
+                    print('posters2html: generating img thumbnail for pdf ' + filename)
+                    doc = fitz.open(os.path.join(poster_folder,filename))
+                    thumbnail = doc[0].get_pixmap()
+                    thumbnail_name = filename.split('.')[0] + '.png'
+                    thumbnail.save(os.path.join(poster_folder,'Thumbnails/',thumbnail_name))
                 break
         if img_path:
-            f.write('<a target="_blank" href="' + img_path + '"><img src="' + img_path + '" class="card-img-top"></a>\n')
+            if img_path.endswith('.pdf'):
+                thumbnail_name = img_path.split('/')[-1].split('.')[0] + '.png'
+                thumbnail_path = os.path.join(poster_folder, 'Thumbnails/',thumbnail_name)
+                f.write('<a target="_blank" href="' + img_path + '"><img src="' + thumbnail_path + '" class="card-img-top poster-img"></a>\n')
+            else:
+                f.write('<a target="_blank" href="' + img_path + '"><img src="' + img_path + '" class="card-img-top poster-img"></a>\n')
+        else:
+            print('posters2html: could not find poster ' + str(row['Poster_Code']))
 
         # write body
         f.write('<div class="card-body">\n')
